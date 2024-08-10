e8311ee3597be4d47778a02ec99c3d7435bacddf
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2024-08-09 21:32:50 -0500

    voice

---
+++ [voice]/polvigerowbau/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/polvigerowb_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/polvigerowb_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_polvigerowb'
+
+
+
+
+
