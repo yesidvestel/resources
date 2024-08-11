58c84787358dbcc5d6df06fdc3b3de31b3e435e1
Emflow3845Q <128913713+Emflow3845Q@users.noreply.github.com>
2024-08-11 09:06:18 -0500

    V 1.2
    * Se deshabilita el mapa del hospital que viene por defecto.
    
    * Se agrega nuevo mapeado de hospital

---
+++ [defaultmaps]/hopital/README.txt
@@ -0,0 +1,3 @@
+Salut si vous recherchez des mapping ou script rare rejoingnez se discord : https://discord.gg/ct6QtxH4Fp !!!
+
+Hi if you are looking for mapping or rare script join this discord: https://discord.gg/ct6QtxH4Fp!!!
---
+++ [defaultmaps]/hopital/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+version '1.0.2'
+
+files {
+  'audio/v_diables_hopital_col_game.dat151.rel',
+  'audio/v_diables_hopital_col_mix.dat15.rel',
+  'audio/v_diables_hopital_col_part2_game.dat151.rel',
+  'audio/v_diables_hopital_col_part2_mix.dat15.rel',
+}
+
+
+data_file 'AUDIO_GAMEDATA' 'audio/v_diables_hopital_col_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_diables_hopital_col_mix.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/v_diables_hopital_col_part2_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/v_diables_hopital_col_part2_mix.dat'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/interior/ytyp/v_diables_hopital.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/interior/part2/v_diables_hopital_part2.ytyp'
