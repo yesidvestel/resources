1d0012a84bb0cbcdb0207875f7f73a571a8b0764
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2024-08-11 08:24:21 -0500

    V 1.1
    * Menu de fps

---
+++ [standalone]/BakiTelli_fpsmenu/config.lua
@@ -0,0 +1,13 @@
+
+Config = {}
+
+
+Config.ServerName = "ComunaRp" -- # SERVER NAME # --
+
+Config.SubName = "MENÚ FPS" -- # NAME # --
+
+Config.Discord = "https://discord.gg/M6BCbHBW3t" -- # DISCORD URL # --
+
+Config.Web = "https://acortar.link/gyYiQZ" -- # WEB URL # --
+
+Config.Command = "fpsmenu" -- # FPS MENU OPEN COMMAND # --
