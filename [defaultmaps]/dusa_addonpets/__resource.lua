699c22932a55bc1d584a92259eb22bff2c9e0a3b
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2024-08-12 19:34:47 -0500

    V 1.11
    * Agregados pets faltantes de mascotas

---
+++ [defaultmaps]/dusa_addonpets/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+author 'dusadev.tebex.io'
+description 'Dusa Pet Addons'
+
+files {
+    'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/props/leash/leash_model.ytyp'
