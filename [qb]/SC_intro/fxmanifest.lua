0e88b3211ec784b69a610cfd135e79a5e40db3fc
Emflow3845Q <128913713+Emflow3845Q@users.noreply.github.com>
2024-08-27 11:20:00 -0500

    V 1.43
    * Eliminación favelas
    * Se agrega nuevo loading

---
+++ [qb]/SC_intro/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'FadeCloud.store'
+
+files {
+	'html/**/',
+}
+
+loadscreen 'html/index.html'
+
---
+++ [qb]/SC_intro/html/config.js
@@ -0,0 +1,49 @@
+/*
+    IMAGE SIZES 
+
+    Logo - 170x170 px
+    Team - 160x160 px 
+*/
+
+Config = {
+    color: '#0DFF00',
+    infoText: 'Sigue las reglas y verás que nada se interpondrá en tu camino hacia la grandeza. Únete a nuestro Discord para estar siempre al tanto de las novedades, eventos y cambios. ¡No te pierdas ninguna actualización y vive la experiencia al máximo!',
+    useVideoBackground: true,
+    autoSongPlay: true,
+    videoLink: 'https://movieflix.com.co/SERIES/ACCION/Invencible/loading.mp4',
+    mp3Link: 'https://www.dropbox.com/scl/fi/okeoinrehzm31itk1arz2/J-Balvin-Willy-William-Mi-Gente-Official-Video.mp3?rlkey=a0eg88jt6e1dz1jo17jrajwt3&e=1&st=m3d088bo&dl=1',
+    copy: {
+        left: {
+            link: '', 
+            text: '33leaks',
+        }, // left copyfield
+        middle: {
+            link: 'discord.gg/33leaks', 
+            text: 'discord.gg/33leaks',
+        }, // middle copyfield
+        right: {
+            link: '', 
+            text: 'n1klaspipe#2708',
+        }, // right copyfield
+    },
+    team: [
+        {
+            discord: 'emflow4989',
+            nickname: 'Emflow',
+            teamrole: 'Fundador',
+            icon: 'https://cdn.discordapp.com/attachments/840810737298702406/1277992318456037376/foto.png?ex=66cf2e98&is=66cddd18&hm=aa8438101db016b3f15b75323b5ff597e9b6659f1f62f6e5966f403a39f6f0f7&',
+        },
+        {
+            discord: 'DJCION',
+            nickname: 'djcion',
+            teamrole: 'Fundador',
+            icon: 'https://cdn.discordapp.com/attachments/840810737298702406/1277992318456037376/foto.png?ex=66cf2e98&is=66cddd18&hm=aa8438101db016b3f15b75323b5ff597e9b6659f1f62f6e5966f403a39f6f0f7&',
+        },
+        {
+            discord: 'xMike.',
+            nickname: 'xmike.',
+            teamrole: 'Fundador',
+            icon: 'https://cdn.discordapp.com/attachments/840810737298702406/1277994505370468443/miguel.jpg?ex=66cf30a2&is=66cddf22&hm=5fbdc5d71f891940f72255034adce50382761f1cfb4b9629cccfb789accea58c&',
+        },
+    ],
+};
